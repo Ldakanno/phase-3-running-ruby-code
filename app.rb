18b8da8108774c10246fc16f3e94e7ffc9374c0e
Lianne Dakanno <liannedakanno@gmail.com>
2023-03-09 20:09:20 -0600

    commit tree 65c0c5161353ce6a22999a114cc318619434707b

--- app.rb
+++ app.rb
@@ -1,23 +1,39 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
-#     { id: 4, hello: "Learner" }]
+# app.rb
+puts "Hello world!"
+puts("Hello world!")
 
-# p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
 
-# def add
-    
-# end
-# # 1 + 1
-# puts add + add
+puts "Hello world!"
+puts "Hello world!"
+puts "Hello world!"
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+print "Hello world!"
+print "Hello world!"
+print "Hello world!"
 
-puts add(1,3) + add(2,2)
+puts [1, 2, 3]
+
+p [1, 2, 3]
+
+pp [
+    { 
+        id: 1,
+        hello: "World"
+    }, 
+    {
+        id: 2,
+        hello: "Ruby"
+    }, 
+    {
+        id: 3,
+        hello: "Moon"
+    }, 
+    {
+        id: 4,
+        hello: "Learner"
+    }
+]
+
+puts "Hello World!"
+
+print "Pass this test, please."
